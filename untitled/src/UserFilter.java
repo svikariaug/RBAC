0850b60031e27f40d6f90e01f8712d375876c9cb
svikariaug <anastasiasagalieva@gmail.com>
2026-02-25 23:12:23 +0700

    2.1 subparagraph

--- untitled/src/UserFilter.java
+++ untitled/src/UserFilter.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+import java.util.function.Predicate;
 
-public class UserFilter {
+@FunctionalInterface
+public interface UserFilter extends Predicate<User> {
+    @Override
+    boolean test(User user);
+
+    default UserFilter and(UserFilter other) {
+        return user -> this.test(user) && other.test(user);
+    }
+
+    default UserFilter or(UserFilter other) {
+        return user -> this.test(user) || other.test(user);
+    }
 }
