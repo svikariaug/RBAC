2638a15d00a6cca76b49869a72c6fdf739c3be12
svikariaug <anastasiasagalieva@gmail.com>
2026-05-10 00:09:39 +0700

    :second

--- untitled/src/AuditLogTest.java
+++ untitled/src/AuditLogTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import java.util.concurrent.TimeUnit;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AuditLogTest {
@@ -10,9 +11,12 @@ class AuditLogTest {
         log.log("ROLE_CREATE", "admin", "MANAGER", "created");
         log.log("USER_DELETE", "system", "x", "deleted");
 
+        assertTrue(log.flush(2, TimeUnit.SECONDS));
         assertEquals(3, log.getAll().size());
         assertEquals(2, log.getByPerformer("admin").size());
         assertEquals(1, log.getByAction("USER_DELETE").size());
+
+        log.shutdownAndAwait(1, TimeUnit.SECONDS);
     }
 }
 
